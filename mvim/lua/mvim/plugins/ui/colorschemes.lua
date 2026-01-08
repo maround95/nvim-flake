420414551158eadaf13363c33f4ebe4cc522ad12
Maroun Deeb <maround95@gmail.com>
2026-01-08 20:04:25 +0100

    Fix: Make visual highlight group legible

--- mvim/lua/mvim/plugins/ui/colorschemes.lua
+++ mvim/lua/mvim/plugins/ui/colorschemes.lua
@@ -1,22 +1,23 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-		lazy = true,
-		opts = {
-			options = {
-				-- transparent = true,
-			},
-			groups = {
-				all = {
-					FloatBorder = { fg = "fg3", bg = "bg0" },
-					FloatTitle = { fg = "syntax.func", bg = "bg0" },
-				},
-			},
-		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = true,
-		opts = { style = "moon" },
-	},
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = true,
+    opts = {
+      options = {
+        -- transparent = true,
+      },
+      groups = {
+        all = {
+          FloatBorder = { fg = "fg3", bg = "bg0" },
+          FloatTitle = { fg = "syntax.func", bg = "bg0" },
+          Visual = { bg = "palette.orange.dim" },
+        },
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
 }
